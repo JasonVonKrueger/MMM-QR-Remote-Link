27da9e5e207591d785098086deff93d2593d2bbb
JasonVonKrueger <rpoore@gmail.com>
2023-11-03 20:55:40 -0400

    initial

---
+++ MMM-QR-Remote-Link.js
@@ -0,0 +1,54 @@
+Module.register('MMM-QR-Remote-Link', {
+    defaults: {
+
+    },
+
+    start() {
+        this.id = Math.floor(Math.random() * 9);
+      Log.info("Starting module: " + this.name + " with identifier: " + this.identifier);
+    },
+
+    getStyles() {
+      return [];
+    },
+
+    getScripts: function() {
+        return [];
+    },
+
+    getDom() {
+      const wrapper = document.createElement('div');
+
+      let markup = `
+          <div style="text-align: center">
+            <div><img id="qrcode_${this.identifier}" /></div>
+            <div id="qrtext_${this.identifier}" style="font-size: .8rem; color: #eee;"></div>
+          </div>
+      `;
+
+      wrapper.innerHTML = markup;
+      return wrapper;
+    },
+
+    notificationReceived(notification, payload, sender) {
+      this.sendSocketNotification('QRCODE_INIT', this.config.endpoint);
+
+      if (sender) {
+        Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+      } else {
+        Log.log(this.name + " received a system notification: " + notification);
+      }
+    },
+
+    socketNotificationReceived(notification, payload) {
+        if (notification === "QRCODE_READY") {
+            document.querySelector('#qrcode_' + this.identifier).src = payload;
+            document.querySelector('#qrcode_' + this.identifier).style.width = this.config.size + 'px';
+            document.querySelector('#qrcode_' + this.identifier).style.height = this.config.size + 'px';
+            document.querySelector('#qrtext_' + this.identifier).innerHTML = `<i>${this.config.text}</i>`;
+        }
+ 
+       // Log.log(this.name + " received a fat socket notification: " + notification + " - Payload: " + JSON.stringify(payload));
+    },
+
+  })
---
+++ node_helper.js
@@ -0,0 +1,34 @@
+const NodeHelper = require("node_helper");
+const os = require("os");
+const QRCode = require('qrcode');
+
+module.exports = NodeHelper.create({
+    start: function () {},
+
+    createRoutes: function () {
+        var self = this;
+    },
+
+    getIP: function () {
+        let nic = os.networkInterfaces();
+        return nic.wlan0[0].address.toString();
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        var self = this;
+
+        if (notification === 'QRCODE_INIT') {
+            // send QR code of remote's URL
+            QRCode.toDataURL(`http://${this.getIP()}:${config.port}${payload}`)
+                .then(url => {
+                    self.sendSocketNotification("QRCODE_READY", url);
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+
+            //self.sendSocketNotification("IP_ADDRESSES", this.buildQRCode());
+            console.log('socket notification received');
+        }
+    }
+});
