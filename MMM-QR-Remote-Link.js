59bc207fbbd065bcbda751bca329c0561f527c87
JasonVonKrueger <rpoore@gmail.com>
2024-02-04 12:35:37 -0500

    Add files via upload
    Fixed the issue where the generated QR code content was the same for different instances of this module.

--- MMM-QR-Remote-Link.js
+++ MMM-QR-Remote-Link.js
@@ -2,14 +2,13 @@ Module.register('MMM-QR-Remote-Link', {
     defaults: { },
 
     start() {
-        // create an id for the element so more than one can be rendered...probably need something better
-        this.id = Math.floor(Math.random() * 9);
-        
-        Log.info("Starting module: " + this.name + " with identifier: " + this.identifier);
+      Log.info('Starting module: ' + this.name + ' with identifier: ' + this.identifier);
+
+      this.sendSocketNotification('QRCODE_INIT', { id: this.identifier, endpoint: this.config.endpoint });
     },
 
     getStyles() {
-        return [];
+      return [];
     },
 
     getScripts: function() {
@@ -17,32 +16,31 @@ Module.register('MMM-QR-Remote-Link', {
     },
 
     getDom() {
-        const wrapper = document.createElement('div');
-        
-        let markup = `
+      const wrapper = document.createElement('div');
+
+      let markup = `
           <div style="text-align: center">
             <div><img id="qrcode_${this.identifier}" /></div>
             <div id="qrtext_${this.identifier}" style="font-size: .8rem; color: #eee;"></div>
           </div>
-        `;
-        
-        wrapper.innerHTML = markup;
-        return wrapper;
+      `;
+
+      wrapper.innerHTML = markup;
+      return wrapper;
     },
 
     notificationReceived(notification, payload, sender) {
-        this.sendSocketNotification('QRCODE_INIT', this.config.endpoint);
-        
-        if (sender) {
-            Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
-        } else {
-            Log.log(this.name + " received a system notification: " + notification);
-        }
+      if (sender) {
+        Log.log(this.name + ' received a module notification: ' + notification + ' from sender: ' + sender.name);
+      }
+      else {
+        Log.log(this.name + ' received a system notification: ' + notification);
+      }
     },
 
     socketNotificationReceived(notification, payload) {
-        if (notification === "QRCODE_READY") {
-            document.querySelector('#qrcode_' + this.identifier).src = payload;
+        if (notification === 'QRCODE_READY' && payload.id === this.identifier) {
+            document.querySelector('#qrcode_' + this.identifier).src = payload.url;
             document.querySelector('#qrcode_' + this.identifier).style.width = this.config.size + 'px';
             document.querySelector('#qrcode_' + this.identifier).style.height = this.config.size + 'px';
             document.querySelector('#qrtext_' + this.identifier).innerHTML = `<i>${this.config.text}</i>`;
--- node_helper.js
+++ node_helper.js
@@ -1,32 +1,21 @@
-const NodeHelper = require("node_helper");
-const os = require("os");
-const QRCode = require('qrcode');
+const NodeHelper = require('node_helper')
+const os = require('os')
+const QRCode = require('qrcode')
 
 module.exports = NodeHelper.create({
-    start: function () {},
+    start: function() {},
 
-    createRoutes: function () {
-        var self = this;
-    },
-
-    getIP: function () {
-        // this is assuming a wifi connection and probably a Raspberry PI is used
-        let nic = os.networkInterfaces();
-        return nic.wlan0[0].address.toString();
+    getIP: function() {
+        let nic = os.networkInterfaces()
+        return nic.wlan0[0].address.toString()
     },
 
     socketNotificationReceived: function(notification, payload) {
-        var self = this;
-
         if (notification === 'QRCODE_INIT') {
-            // send QR code of remote's URL
-            QRCode.toDataURL(`http://${this.getIP()}:${config.port}${payload}`)
-                .then(url => {
-                    self.sendSocketNotification("QRCODE_READY", url);
-                })
-                .catch(err => {
-                    console.error(err)
-                })
+            // send QR code of remote URL
+            QRCode.toDataURL(`http://${this.getIP()}:${config.port}${payload.endpoint}`)
+                .then(url => { this.sendSocketNotification('QRCODE_READY', { id: payload.id, url: url }) })
+                .catch(err => { console.error(err) })
         }
     }
 });
